e22d27ab0f7bcba6d02ca3d48471ac22d83bee71
NRO1 <nrdevac1@gmail.com>
2022-12-01 14:04:24 +0200

    first

---
+++ screens/StartScreen.js
@@ -0,0 +1,11 @@
+import  { TextInput, View } from 'react-native';
+
+function StartScreen() {
+    return (
+        <View>
+            <TextInput />
+        </View>
+    )
+}
+
+export default StartScreen;
